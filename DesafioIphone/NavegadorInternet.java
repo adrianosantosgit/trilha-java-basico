a854ca3eafb0c2153fab0b46540171a81ca350f6
adrianosantosgit <adriansants@msn.com>
2024-06-27 12:14:11 -0300

    commit desafio iPhone Java Santander 2024

---
+++ DesafioIphone/NavegadorInternet.java
@@ -0,0 +1,11 @@
+public interface NavegadorInternet {
+    public static void exibirPagina(String string){
+        System.out.println("EXIBINDO PAGINA" + string);
+    }
+    public static void adicionarNovaAba(){
+        System.out.println("ADICIONANDO NOVA ABA");
+    }
+    public static void atualizarPagina(){
+        System.out.println("ATUALIZANDO PAGINA");
+    }
+}
---
+++ DesafioIphone/iPhone.java
@@ -0,0 +1,5 @@
+public class iPhone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical{
+    public static void main(String[] args) {
+        
+    }
+}
